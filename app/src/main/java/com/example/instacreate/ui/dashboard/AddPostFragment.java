cd4bf76f3ef755728017fe2a211fe6958836f48f
zayadi <zayadi@clappingape.com>
2020-03-25 20:09:45 +0700

    first commit

---
+++ app/src/main/java/com/example/instacreate/ui/dashboard/AddPostFragment.java
@@ -0,0 +1,52 @@
+package com.example.instacreate.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.instacreate.R;
+
+public class AddPostFragment extends Fragment {
+
+    private AddPostViewModel addPostViewModel;
+    private TextView textView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_post, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = view.findViewById(R.id.text_dashboard);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity() != null){
+            addPostViewModel = ViewModelProviders.of(this).get(AddPostViewModel.class);
+            observeViewModel();
+        }
+    }
+
+    private void observeViewModel() {
+        addPostViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+    }
+}
